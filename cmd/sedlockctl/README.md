31cc21290f0aee061700c55e9f04eaa066800364
Christopher Meis <christopher.meis@9elements.com>
2022-04-13 10:19:41 +0200

    cmd/sedlockctl: * Replace flag parsing with alecthomas/kong command line parser for better usage and code quality
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- cmd/sedlockctl/README.md
+++ cmd/sedlockctl/README.md
@@ -1,6 +1,28 @@
 # sedlockctl
 
 Used to operate SED locking from a Linux shell.
+```
+Usage: sedlockctl --device=STRING command
+
+Go SEDlock control (temporary name)
+
+Flags:
+  -h, --help                  Show context-sensitive help.
+  -d, --device=STRING         Path to SED device (e.g. /dev/nvme0)
+      --sidpin=STRING
+      --sidpinmsid
+      --sidhash=STRING
+  -u, --user=STRING
+  -p, --password=STRING
+      --hash="sedutil-dta"
+
+Commands:
+  list          List all ranges (default)
+  lock-all      Locks all ranges completely
+  unlock-all    Unlocks all ranges completely
+  mbrdone       Sets the MBRDone property (hide/show Shadow MBR)
+  read-mbr      Prints the binary data in the MBR area
+```
 
 Example:
 
@@ -30,7 +52,7 @@ Range   7: disabled
 Range   8: disabled
 $ sudo fdisk -l /dev/sdd
 Disk /dev/sdd: 465.76 GiB, 500107862016 bytes, 976773168 sectors
-Disk model: Samsung SSD 860 
+Disk model: Samsung SSD 860
 Units: sectors of 1 * 512 = 512 bytes
 Sector size (logical/physical): 512 bytes / 512 bytes
 I/O size (minimum/optimal): 512 bytes / 512 bytes
---
+++ cmd/sedlockctl/cmd.go
@@ -0,0 +1,134 @@
+// Copyright (c) 2022 by library authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/open-source-firmware/go-tcg-storage/pkg/core/table"
+	"github.com/open-source-firmware/go-tcg-storage/pkg/locking"
+)
+
+type context struct {
+	session *locking.LockingSP
+}
+
+type listCmd struct{}
+
+type lockAllCmd struct{}
+
+type unlockAllCmd struct{}
+
+type mbrDoneCmd struct {
+	Stat bool `required:"" help:"Status to set the MBRDone"`
+}
+
+type readMBRCmd struct {
+	ReadMbrSize int `flag:"" default:"0"`
+}
+
+var cli struct {
+	Device     string       `flag:"" required:"" short:"d"  help:"Path to SED device (e.g. /dev/nvme0)"`
+	Sidpin     string       `flag:"" optional:""`
+	Sidpinmsid bool         `flag:"" optional:""`
+	Sidhash    string       `flag:"" optional:""`
+	User       string       `flag:"" optional:"" short:"u"`
+	Password   string       `flag:"" optional:"" short:"p"`
+	Hash       string       `flag:"" optional:"" default:"sedutil-dta"`
+	List       listCmd      `cmd:"" help:"List all ranges (default)"`
+	LockAll    lockAllCmd   `cmd:"" help:"Locks all ranges completely"`
+	UnlockAll  unlockAllCmd `cmd:"" help:"Unlocks all ranges completely"`
+	Mbrdone    mbrDoneCmd   `cmd:"" help:"Sets the MBRDone property (hide/show Shadow MBR)"`
+	ReadMbr    readMBRCmd   `cmd:"" help:"Prints the binary data in the MBR area"`
+}
+
+func (l listCmd) Run(ctx *context) error {
+	if len(ctx.session.Ranges) == 0 {
+		return fmt.Errorf("no available locking ranges as this user")
+	}
+	for i, r := range ctx.session.Ranges {
+		strr := "whole disk"
+		if r.End > 0 {
+			strr = fmt.Sprintf("%d to %d", r.Start, r.End)
+		}
+		if !r.WriteLockEnabled && !r.ReadLockEnabled {
+			strr = "disabled"
+		} else {
+			if r.WriteLocked {
+				strr += " [write locked]"
+			}
+			if r.ReadLocked {
+				strr += " [read locked]"
+			}
+		}
+		if r == ctx.session.GlobalRange {
+			strr += " [global]"
+		}
+		if r.Name != nil {
+			strr += fmt.Sprintf(" [name=%q]", *r.Name)
+		}
+		fmt.Printf("Range %3d: %s\n", i, strr)
+	}
+	return nil
+}
+
+func (u unlockAllCmd) Run(ctx *context) error {
+	for i, r := range ctx.session.Ranges {
+		if err := r.UnlockRead(); err != nil {
+			return fmt.Errorf("read unlock range %d failed: %v", i, err)
+		}
+		if err := r.UnlockWrite(); err != nil {
+			return fmt.Errorf("write unlock range %d failed: %v", i, err)
+		}
+	}
+	return nil
+}
+
+func (l lockAllCmd) Run(ctx *context) error {
+	for i, r := range ctx.session.Ranges {
+		if err := r.LockRead(); err != nil {
+			return fmt.Errorf("read lock range %d failed: %v", i, err)
+		}
+		if err := r.LockWrite(); err != nil {
+			return fmt.Errorf("write lock range %d failed: %v", i, err)
+		}
+	}
+	return nil
+}
+
+func (m mbrDoneCmd) Run(ctx *context) error {
+	if err := ctx.session.SetMBRDone(m.Stat); err != nil {
+		return fmt.Errorf("SetMBRDone failed: %v", err)
+	}
+	return nil
+}
+
+func (r readMBRCmd) Run(ctx *context) error {
+	mbi, err := table.MBR_TableInfo(ctx.session.Session)
+	if err != nil {
+		return fmt.Errorf("table.MBR_TableInfo failed: %v", err)
+	}
+	mbuf := make([]byte, mbi.SuggestBufferSize(ctx.session.Session))
+	sz := mbi.Size
+	if r.ReadMbrSize > 0 && uint32(r.ReadMbrSize) < sz {
+		sz = uint32(r.ReadMbrSize)
+	}
+	pos := uint32(0)
+	chk := uint32(len(mbuf))
+	for i := sz; i != 0; i -= chk {
+		if n, err := table.MBR_Read(ctx.session.Session, mbuf, pos); n != len(mbuf) || err != nil {
+			return fmt.Errorf("table.MBR_Read failed: %v (read: %d)", err, n)
+		}
+		pos += chk
+		if i < chk {
+			os.Stdout.Write(mbuf[:i])
+			break
+		} else {
+			os.Stdout.Write(mbuf)
+		}
+	}
+	return nil
+}
